99514d3844b9ca35e5f8fc1b2dc0e90cf0bcc059
Nathan Guianvarc'h <nathanguianvarch@gmail.com>
2024-06-12 16:06:21 +0200

    Ajouts des réseaux sociaux

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,30 @@ const nextConfig = {
       },
     ],
   },
+  redirects() {
+    return [
+      {
+        source: "/instagram",
+        destination: "https://www.instagram.com/nathanguianvarch/",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/nathanguianvarch/",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://www.github.com/nathanguianvrch/",
+        permanent: true,
+      },
+      {
+        source: "/mail",
+        destination: "mailto:nathanguianvarch@gmail.com",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
